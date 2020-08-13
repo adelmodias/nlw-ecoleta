cdbae549a410a41d32f37a6d8bd2dfbcef4bb0a2
Adelmo Dias <adelmodias@outlook.com>
2020-08-13 19:57:39 -0300

    SQLite configured using knex.js

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,9 @@
 import express, { response } from "express";
+import routes from "./routes";
 
 const app = express();
 
-// Para o Express conseguir ler e entender uma request body no formato JSON.
 app.use(express.json());
-
-const users = ["Adelmo", "Diego", "Rodrigo", "André", "Maurício"];
-
-app.get("/users", (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search
-        ? users.filter((user) => user.includes(search))
-        : users;
-
-    response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-    const id = Number(request.params.id);
-    response.json(users[id]);
-});
-
-app.post("/users", (request, response) => {
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email,
-    };
-
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
